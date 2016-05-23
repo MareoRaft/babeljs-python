f71a124244dfba5a15c0083db7e1b21d07caf964
Mareo Raft <mareoraft@gmail.com>
2016-05-23 10:52:44 -0400

    Added `out_path` option to transform functions
    A use can specify a path if they want their transpiled JS to output to a file.  For example:
    
    t = babeljs.transformer
    t.transform('my/js-6/index.js', 'my/js/index.js')
    
    and the file my/js/index.js will be created with the appropriate output.

--- babeljs/transformer.py
+++ babeljs/transformer.py
@@ -19,20 +19,25 @@ class Transformer(object):
         except:
             raise TransformError()
 
-    def transform_string(self, js, **opts):
+    def transform_string(self, js, out_path=None, **opts):
         try:
-            return self.context.call('babel.transform', js, opts)
+            out = self.context.call('babel.transform', js, opts)
         except execjs.ProgramError as e:
             raise TransformError(e.message[7:])
+        if out_path:
+            with open(out_path, 'w') as f:
+                f.write(out)
+        return out
 
-    def transform(self, js_path, **opts):
-        with open(js_path, 'r') as f:
+    def transform(self, in_path, **opts):
+        with open(in_path, 'r') as f:
             return self.transform_string(f.read(), **opts)
 
 
-def transform(js_path, **opts):
-    return Transformer().transform(js_path, **opts)
+
+def transform(*args, **opts):
+    return Transformer().transform(*args, **opts)
 
 
-def transform_string(js, **opts):
-    return Transformer().transform_string(js, **opts)
+def transform_string(*args, **opts):
+    return Transformer().transform_string(*args, **opts)
